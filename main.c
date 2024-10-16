0777e73cc0f4c12f5f1189adb914aa649dd9588c
akhand <akhand3108patel@gmail.com>
2024-10-16 23:55:37 +0530

    Update main.c to implement loading program from file and add SET instruction

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
-
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#define STACK_SIZE 256
+
+// Macros for register access
 #define sp (registers[SP])
 #define ip (registers[IP])
 
-typedef enum{
-  PSH,
-  ADD,
-  POP,
-  SET,
-  HLT
+// Updated InstructionSet enum
+typedef enum {
+    PSH = 0,
+    POP = 1,
+    ADD = 2,
+    SET = 3,
+    HLT = 4
 } InstructionSet;
 
 typedef enum {
@@ -18,57 +22,81 @@ typedef enum {
     NUM_OF_REGISTERS
 } Registers;
 
-const int program[] = {
-  PSH, 5,
-  PSH, 6,
-  ADD,
-  POP,
-  HLT
-};
-
-
+// Global variables
 bool running = true;
-int stack[256];
+int stack[STACK_SIZE];
 int registers[NUM_OF_REGISTERS];
 
+// Function to load the program from a file
+int* load_program(const char* filename, int* program_size) {
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
 
-int fetch(){
-  return program[ip];
+    int* program = malloc(sizeof(int) * STACK_SIZE);
+    int i = 0;
+
+    while (fscanf(file, "%d", &program[i]) != EOF) {
+        i++;
+    }
+
+    fclose(file);
+    *program_size = i;
+    return program;
 }
 
-
-void eval(int instr){
-  switch(instr){
-    case HLT:{
-      running = false;
-      break;
-    }
-    case PSH:{
-      sp++;
-      stack[sp] = program[++ip];
-      break;
-    }
-    case POP:{
-      int val_popped = stack[sp--];
-      printf("popped %d\n", val_popped);
-      break;
-    }
-    case ADD:{
-      int a = stack[sp--];
-      int b = stack[sp--];
-      int result = b + a;
-      sp++;
-      stack[sp] = result;
-      break;
-    }
-  }
+// Fetch the current instruction
+int fetch(const int* program) {
+    return program[ip];
 }
 
+// Evaluate the instruction
+void eval(int instr, const int* program) {
+    switch (instr) {
+        case HLT:
+            running = false;
+            break;
+        case PSH:
+            sp++;
+            stack[sp] = program[++ip];
+            break;
+        case POP:
+            printf("popped %d\n", stack[sp--]);
+            break;
+        case ADD: {
+            int a = stack[sp--];
+            int b = stack[sp--];
+            stack[++sp] = b + a;
+            break;
+        }
+        case SET:
+            printf("SET is not yet implemented.\n");
+            break;
+        default:
+            printf("Unknown instruction: %d\n", instr);
+            running = false;
+    }
+}
 
-int main(){
-  while(running) { 
-    eval(fetch());
-    ip++;
-  }
-  return 0;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <program file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int program_size;
+    int* program = load_program(argv[1], &program_size);
+
+    registers[IP] = 0;
+    registers[SP] = -1;
+
+    while (running && registers[IP] < program_size) {
+        eval(fetch(program), program);
+        registers[IP]++;
+    }
+
+    free(program);
+    return 0;
 }
